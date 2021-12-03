fee755adf591105e89fcb634f75a4feb4a94125c
Andre <andresc50082@gmail.com>
2021-12-03 11:04:57 -0600

    Administardor, cita y Consultorio

--- src/main/java/com/equipo02/backend/model/Consultorio.java
+++ src/main/java/com/equipo02/backend/model/Consultorio.java
@@ -49,4 +49,36 @@ public class Consultorio implements Serializable {
     public void setHora(String hora) {
         Hora = hora;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
