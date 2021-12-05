cff3f4394932062126e4b5e36295cfbb93f799af
Mafer Villegas <mafer@onetap.mx>
2021-12-05 00:06:29 -0600

    Se solucionaron errores que no permitían correr el código

--- src/main/java/com/equipo02/backend/dao/AdministradorDAO.java
+++
@@ -1,8 +0,0 @@
-package com.equipo02.backend.dao;
-
-
-import com.equipo02.backend.model.Administrador;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdministradorDAO extends CrudRepository<Administrador, Integer> {
-}
